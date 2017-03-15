ce6bef4cf8b020eb246665bde50e835a04391707
Henrique Noronha Facioli <ra157986@beatles.lab.ic.unicamp.br>
2017-03-15 20:38:47 -0300

    Submodules working

--- benchmark.sh
+++ benchmark.sh
@@ -2,21 +2,25 @@
 
 # Compile x264 if not yet
 if ! [ -f ./x264/.example.c ]; then 
-    git submodule update --recursive --remote
+    echo Downloanding x264
+    git submodule update --init --recursive
 fi
-if ! [ -f ./x264/x264 ]; then 
+if ! [ -f ./x264/x264 ]; then
+    echo Compiling x264
     (cd ./x264 && ./configure --disable-asm)
     (cd ./x264 && make)
 fi
 
 # Download youtube-downloader
 if ! [ -f youtube-downloader ]; then
+    echo Downloading youtube downloader
     curl -L https://yt-dl.org/downloads/latest/youtube-dl -o  youtube-downloader
     chmod a+rx youtube-downloader
 fi
 
 # Download video from youtube
 if ! [ -f video.mp4 ]; then
+    echo Downloading Big Bucks Bunny
     ./youtube-downloader https://www.youtube.com/watch\?v\=YE7VzlLtp-4 --youtube-skip-dash-manifest -f 136 -o video.mp4
 fi
 
