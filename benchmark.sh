d39b61bd918a59fe227c0b53ca25ab4f0f0324e3
Henrique Noronha Facioli <ra157986@beatles.lab.ic.unicamp.br>
2017-03-15 20:44:46 -0300

    benchmark preparation Ok

--- benchmark.sh
+++ benchmark.sh
@@ -1,14 +1,16 @@
 # !/bin/bash/
 
 # Compile x264 if not yet
-if ! [ -f ./x264/.example.c ]; then 
+if ! [ -f ./x264/configure ]; then 
     echo Downloanding x264
     git submodule update --init --recursive
 fi
-if ! [ -f ./x264/x264 ]; then
+if ! [ -f ./x264-bin ]; then
     echo Compiling x264
     (cd ./x264 && ./configure --disable-asm)
     (cd ./x264 && make)
+    mv ./x264/x264 ./x264-bin 
+    (cd ./x264 && make clean)
 fi
 
 # Download youtube-downloader
